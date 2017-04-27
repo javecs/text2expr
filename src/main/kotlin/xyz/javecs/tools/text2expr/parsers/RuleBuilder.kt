7376af6ab615c51654720d5b206c0cc5e24b73b3
javecs <moonhee.javecs@gmail.com>
2017-04-28 07:33:51 +0900

    ファイルを分けました。

--- src/main/kotlin/xyz/javecs/tools/text2expr/parsers/RuleBuilder.kt
+++ src/main/kotlin/xyz/javecs/tools/text2expr/parsers/RuleBuilder.kt
@@ -1,58 +1,17 @@
 package xyz.javecs.tools.text2expr.parsers
 
-import org.antlr.v4.runtime.CharStreams
-import org.antlr.v4.runtime.CommonTokenStream
 import xyz.javecs.tools.expr.Calculator
-import xyz.javecs.tools.text2expr.parser.Text2ExprBaseVisitor
-import xyz.javecs.tools.text2expr.parser.Text2ExprLexer
-import xyz.javecs.tools.text2expr.parser.Text2ExprParser
-
-fun parser(source: String): Text2ExprParser {
-    val charStream = CharStreams.fromString(source)
-    val lexer = Text2ExprLexer(charStream)
-    val tokens = CommonTokenStream(lexer)
-    return Text2ExprParser(tokens)
-}
-
-data class Field(var key: String = "", var value: List<String> = ArrayList())
-class Word(var fields: ArrayList<Field> = ArrayList())
-
-internal class RuleInterpreter : Text2ExprBaseVisitor<Unit>() {
-    var expr = StringBuffer()
-    var rule = ArrayList<Word>()
-
-    override fun visitWordDefine(ctx: Text2ExprParser.WordDefineContext): Unit {
-        rule.add(Word())
-        super.visitWordDefine(ctx)
-    }
-
-    override fun visitWordAssign(ctx: Text2ExprParser.WordAssignContext): Unit {
-        rule.add(Word())
-        super.visitWordAssign(ctx)
-    }
-
-    override fun visitField(ctx: Text2ExprParser.FieldContext): Unit {
-        val field = Field()
-        field.key = ctx.PREFIX().text
-        field.value = ctx.value().text.split("|").toList()
-        rule.last().fields.add(field)
-    }
-
-    override fun visitExpr(ctx: Text2ExprParser.ExprContext): Unit {
-        expr.append(ctx.text)
-    }
-}
 
 data class Evaluation(val value:Number = Double.NaN)
 class RuleBuilder(source: String) {
-    private val interpreter = RuleInterpreter()
+    private val parser = RuleParser()
 
     init {
-        interpreter.visit(parser(source).text2expr())
+        parser.visit(parser(source).text2expr())
     }
 
-    fun rule(): Array<Word> = interpreter.rule.toTypedArray()
-    fun expr(): Array<String> = interpreter.expr
+    fun rule(): Array<Word> = parser.rule.toTypedArray()
+    fun expr(): Array<String> = parser.expr
             .split(System.lineSeparator())
             .filter { it.isNotEmpty() }
             .toTypedArray()
---
+++ src/main/kotlin/xyz/javecs/tools/text2expr/parsers/RuleParser.kt
@@ -0,0 +1,43 @@
+package xyz.javecs.tools.text2expr.parsers
+
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import xyz.javecs.tools.text2expr.parser.Text2ExprBaseVisitor
+import xyz.javecs.tools.text2expr.parser.Text2ExprLexer
+import xyz.javecs.tools.text2expr.parser.Text2ExprParser
+
+data class Field(var key: String = "", var value: List<String> = ArrayList())
+class Word(var fields: ArrayList<Field> = ArrayList())
+
+internal fun parser(source: String): Text2ExprParser {
+    val charStream = CharStreams.fromString(source)
+    val lexer = Text2ExprLexer(charStream)
+    val tokens = CommonTokenStream(lexer)
+    return Text2ExprParser(tokens)
+}
+
+internal class RuleParser : Text2ExprBaseVisitor<Unit>() {
+    var expr = StringBuffer()
+    var rule = ArrayList<Word>()
+
+    override fun visitWordDefine(ctx: Text2ExprParser.WordDefineContext): Unit {
+        rule.add(Word())
+        super.visitWordDefine(ctx)
+    }
+
+    override fun visitWordAssign(ctx: Text2ExprParser.WordAssignContext): Unit {
+        rule.add(Word())
+        super.visitWordAssign(ctx)
+    }
+
+    override fun visitField(ctx: Text2ExprParser.FieldContext): Unit {
+        val field = Field()
+        field.key = ctx.PREFIX().text
+        field.value = ctx.value().text.split("|").toList()
+        rule.last().fields.add(field)
+    }
+
+    override fun visitExpr(ctx: Text2ExprParser.ExprContext): Unit {
+        expr.append(ctx.text)
+    }
+}
