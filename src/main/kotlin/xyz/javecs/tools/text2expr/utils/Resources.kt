a6478236c3dfaf798da9696cd79501016e47c47c
javecs <moonhee.javecs@gmail.com>
2017-04-30 21:46:28 +0900

    全ルールを読み込みしました。

--- src/main/kotlin/xyz/javecs/tools/text2expr/utils/Resources.kt
+++ src/main/kotlin/xyz/javecs/tools/text2expr/utils/Resources.kt
@@ -1,6 +1,16 @@
 package xyz.javecs.tools.text2expr.utils
 
 import com.google.common.io.Resources
+import org.reflections.Reflections
+import org.reflections.scanners.ResourcesScanner
+import java.util.regex.Pattern
+import kotlin.streams.toList
 
+private val reflections = Reflections(null, ResourcesScanner())
 fun read(path: String) = Resources.toString(Resources.getResource(path), Charsets.UTF_8)!!
-fun readRule(name: String) = read("rules/$name.txt")
+fun resources(prefix: String): List<String> {
+    return reflections.getResources(Pattern.compile(".+\\.txt")).stream()
+            .peek{ println(it) }
+            .filter { it.startsWith("$prefix/") }
+            .toList()
+}
--- src/test/kotlin/xyz/javecs/tools/text2expr/test/kotlin/ResourcesTest.kt
+++ src/test/kotlin/xyz/javecs/tools/text2expr/test/kotlin/ResourcesTest.kt
@@ -2,7 +2,6 @@ package xyz.javecs.tools.text2expr.test.kotlin
 
 import org.junit.Test
 import xyz.javecs.tools.text2expr.utils.read
-import xyz.javecs.tools.text2expr.utils.readRule
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -11,11 +10,5 @@ class ResourcesTest {
         val s = read("utils/hello.txt")
         assertTrue(s.startsWith("Hello"))
         assertEquals(4, s.split(System.lineSeparator()).size)
-    }
-
-    @Test fun read2() {
-        val s = readRule("rule1")
-        assertTrue(s.startsWith("x = P2:数"))
-        assertEquals(9, s.split(System.lineSeparator()).size)
     }
 }
