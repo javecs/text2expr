84e4edbb3cd7c3e846dabc0acca48842115520e5
javecs <moonhee.javecs@gmail.com>
2017-05-01 19:59:55 +0900

    リソースとテストパッケージの構成を変更しました。

--- src/test/kotlin/xyz/javecs/tools/text2expr/test/kotlin/Text2ExprTest.kt
+++ src/test/kotlin/xyz/javecs/tools/text2expr/test/kotlin/Text2ExprTest.kt
@@ -7,9 +7,6 @@ import xyz.javecs.tools.text2expr.utils.read
 import kotlin.test.assertEquals
 
 class Text2ExprTest {
-
-    val text2Expr = Text2Expr()
-
     @Test fun parser1() {
         val source = read("parsers/parser1.txt")
         val parser = parser(source)
@@ -17,30 +14,7 @@ class Text2ExprTest {
     }
 
     @Test fun eval1() {
-        assertEquals("1", text2Expr.eval("1"))
-    }
-
-    @Test fun eval2() {
-        assertEquals("15", text2Expr.eval("3 掛ける 5"))
-        assertEquals("16", text2Expr.eval("4 かける 4"))
-        assertEquals("60", text2Expr.eval("30 × 2"))
-        assertEquals("120", text2Expr.eval("24 x 5"))
-    }
-
-    @Test fun eval3() {
-        assertEquals("2.5", text2Expr.eval("5 割る 2"))
-        assertEquals("15", text2Expr.eval("30 わる 2"))
-        assertEquals("7.5", text2Expr.eval("１５ ÷ 2"))
-    }
-
-    @Test fun eval4() {
-        assertEquals("3", text2Expr.eval("1 足す 2"))
-        assertEquals("6", text2Expr.eval("3＋3"))
-    }
-
-    @Test fun eval5() {
-        assertEquals("-3", text2Expr.eval("2 引く 5"))
-        assertEquals("0", text2Expr.eval("3−3"))
+        assertEquals("1", Text2Expr().eval("1"))
     }
 
 }
