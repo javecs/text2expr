a214852235d22d971b90195b2f7a8ea408b7892e
javecs <moonhee.javecs@gmail.com>
2017-05-07 12:40:29 +0900

    分割されたトークンから数字をまとめる関数の雛形を追加しました。

--- src/main/kotlin/xyz/javecs/tools/text2expr/rules/RuleBuilder.kt
+++ src/main/kotlin/xyz/javecs/tools/text2expr/rules/RuleBuilder.kt
@@ -3,6 +3,7 @@ package xyz.javecs.tools.text2expr.rules
 import com.atilika.kuromoji.ipadic.Token
 import com.atilika.kuromoji.ipadic.Tokenizer
 import xyz.javecs.tools.expr.Calculator
+import xyz.javecs.tools.text2expr.utils.mergeNumber
 import xyz.javecs.tools.text2expr.utils.normalize
 
 private val BaseOffset = -1
@@ -54,7 +55,7 @@ class RuleBuilder(source: String, val name: String = "") {
             .toTypedArray()
 
     fun matches(text: String, recognizedId: (id: Pair<String, String>) -> Unit = {}): Pair<Boolean, Double> {
-        val tokens = tokenizer.tokenize(text)
+        val tokens = tokenizer.tokenize(text).mergeNumber()
         var offset = BaseOffset
         var remained = rule().size
         rule().forEach {
---
+++ src/main/kotlin/xyz/javecs/tools/text2expr/utils/Tokens.kt
@@ -0,0 +1,7 @@
+package xyz.javecs.tools.text2expr.utils
+
+import com.atilika.kuromoji.ipadic.Token
+
+fun List<Token>.mergeNumber(): List<Token> {
+    return this
+}
